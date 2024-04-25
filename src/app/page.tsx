efdfca49edeaf57dedf46aa647486ade0fc638d1
Sangwon Ryu <me@sryu.dev>
2024-04-26 00:40:31 +0900

    Add organization statistics (real real final)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default async () => {
       <Heading size='lg'>숭실대학교의 문제해결 통계</Heading>
 
       <Flex flexWrap='wrap' direction={{ base: 'column', md: 'row' }} gap={4} mt={6}>
-        <Statistics text="구성원 수 (BOJ)" value={org.bojUserCount} unit='명' diff={1234} />
+        <Statistics text="구성원 수 (BOJ)" value={org.bojUserCount} unit='명' />
         <Statistics text="구성원 수 (solved.ac)" value={org.userCount} unit='명' />
         <Statistics text="푼 문제" value={org.solvedCount} unit='문제' />
         <Statistics text="기여한 문제" value={org.voteCount} unit='문제' />
